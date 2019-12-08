fc3c45eef654d877c7ff7809a411f40e8145f185
webmilio <therealwebmilio@gmail.com>
2019-12-07 23:59:18 -0500

    Temporarily added back non-localized items.

--- Items/TerramonItem.cs
+++ Items/TerramonItem.cs
@@ -8,6 +8,22 @@ namespace Terramon.Items
 {
     public abstract class TerramonItem : StandardItem
     {
+        protected TerramonItem(string displayName, string tooltip, int width, int height, int value = 0, int defense = 0, int rarity = ItemRarityID.White, int maxStack = 999) :
+            this(
+                new Dictionary<GameCulture, string>()
+                {
+                    { GameCulture.English, displayName }
+                },
+                new Dictionary<GameCulture, string>()
+                {
+                    { GameCulture.English, tooltip }
+                }, 
+                width, height, value, defense, rarity, maxStack
+            )
+        {
+        }
+
+
         protected TerramonItem(Dictionary<GameCulture, string> displayNames, Dictionary<GameCulture, string> tooltips, int width, int height, int value = 0, int defense = 0, int rarity = ItemRarityID.White, int maxStack = 999) :
             base(displayNames, tooltips, width, height, value: value, defense: defense, rarity: rarity, maxStack: maxStack)
         {
