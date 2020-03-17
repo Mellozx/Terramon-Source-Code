d9b0f3284aa4e8b4ebe8e9e40373f706d65f92ef
JamzZz <jamzojamz12@gmail.com>
2020-03-17 17:49:04 -0600

    Revert to good Apricorn sprites

--- Items/Apricorns/WhiteApricorn.cs
+++ Items/Apricorns/WhiteApricorn.cs
@@ -20,13 +20,6 @@ namespace Terramon.Items.Apricorns
             Tooltip.SetDefault("A special fruit seemingly related to berries."
                 + "\nCan be used to craft assorted Poké Balls.");
         }
-
-        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
-        {
-            Texture2D texture = Main.itemTexture[item.type];
-            spriteBatch.Draw(texture, item.Center - Main.screenPosition, null, lightColor, 0f, texture.Size() / 2f, item.scale / 2, SpriteEffects.None, 0);
-            return false;
-        }
         public override void SetDefaults()
         {
             item.width = 28;
