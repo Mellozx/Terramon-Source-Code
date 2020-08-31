6a9dd07135eb1038de74e42fff04a4cbd6e5b9e8
KindRedSand <sand25g@gmail.com>
2020-09-01 01:06:30 +0300

    Fix pokeball statue

--- Tiles/Statues/PokeballStatue.cs
+++ Tiles/Statues/PokeballStatue.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -16,11 +17,12 @@ namespace Terramon.Tiles.Statues
 			Main.tileShine[Type] = 1100;
 			Main.tileSolid[Type] = false;
 			Main.tileSolidTop[Type] = false;
-			Main.tileFrameImportant[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
             minPick = 0;
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
             TileObjectData.newTile.CoordinateHeights = new int[] { 16, 18 };
-            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleHorizontal = false;
 			TileObjectData.newTile.LavaDeath = false;
 			TileObjectData.addTile(Type);
 
@@ -30,6 +32,72 @@ namespace Terramon.Tiles.Statues
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
             Item.NewItem(i * 16, j * 16, 32, 48, mod.ItemType("PokeballStatueItem"));
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+        {
+            Tile tile = Main.tile[i, j];
+
+            if (tile.frameX > 0)
+            {
+                // We can support different light colors for different styles here: switch (tile.frameY / 54)
+                r = 1f;
+                g = 0.4f;
+                b = 0.4f;
+            }
+        }
+
+        public override void HitWire(int i, int j)
+        {
+            Tile tile = Main.tile[i, j];
+            int topY = j - tile.frameY / 18 % 3;
+            short frameAdjustment = (short)(tile.frameX > 0 ? -18*2 : 18*2);
+            Main.tile[i, topY].frameX += frameAdjustment;
+            Main.tile[i, topY + 1].frameX += frameAdjustment;
+            Main.tile[i+1, topY].frameX += frameAdjustment;
+            Main.tile[i+1, topY + 1].frameX += frameAdjustment;
+            //Main.tile[i, topY + 2].frameX += frameAdjustment;
+            Wiring.SkipWire(i, topY);
+            Wiring.SkipWire(i, topY + 1);
+            Wiring.SkipWire(i+1, topY);
+            Wiring.SkipWire(i+1, topY + 1);
+            //Wiring.SkipWire(i, topY + 2);
+            NetMessage.SendTileSquare(-1, i, topY + 1, 2, TileChangeType.None);
+            NetMessage.SendTileSquare(-1, i+1, topY + 1, 2, TileChangeType.None);
+
+        }
+
+        public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref Color drawColor, ref int nextSpecialDrawIndex)
+        {
+            if (!Main.gamePaused && Main.instance.IsActive && (!Lighting.UpdateEveryFrame || Main.rand.NextBool(4)))
+            {
+                Tile tile = Main.tile[i, j];
+                short frameX = tile.frameX;
+                short frameY = tile.frameY;
+                if (Main.rand.NextBool(40) && frameX == 0)
+                {
+                    int style = frameY / 54;
+                    if (frameY / 18 % 3 == 0)
+                    {
+                        int dustChoice = -1;
+                        if (style == 0)
+                        {
+                            dustChoice = 21; // A purple dust.
+                        }
+                        // We can support different dust for different styles here
+                        if (dustChoice != -1)
+                        {
+                            int dust = Dust.NewDust(new Vector2(i * 16 + 4, j * 16 + 2), 4, 4, dustChoice, 0f, 0f, 100, default(Color), 1f);
+                            if (Main.rand.Next(3) != 0)
+                            {
+                                Main.dust[dust].noGravity = true;
+                            }
+                            Main.dust[dust].velocity *= 0.3f;
+                            Main.dust[dust].velocity.Y = Main.dust[dust].velocity.Y - 1.5f;
+                        }
+                    }
+                }
+            }
         }
     }
     
--- Tiles/Statues/PokeballStatueLite.cs
+++ Tiles/Statues/PokeballStatueLite.cs
@@ -31,7 +31,8 @@ namespace Terramon.Tiles.Statues
         public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
         {
             Tile tile = Main.tile[i, j];
-            if (tile.frameX == 0)
+
+            if (tile.frameX>0)
             {
                 // We can support different light colors for different styles here: switch (tile.frameY / 54)
                 r = 1f;
@@ -43,6 +44,20 @@ namespace Terramon.Tiles.Statues
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
             Item.NewItem(i * 16, j * 16, 32, 48, mod.ItemType("PokeballStatueLiteItem"));
+        }
+
+        public override void HitWire(int i, int j)
+        {
+            Tile tile = Main.tile[i, j];
+            int topY = j - tile.frameY / 18 % 2;
+            short frameAdjustment = (short)(tile.frameX > 0 ? -18 : 18);
+            Main.tile[i, topY].frameX += frameAdjustment;
+            Main.tile[i, topY + 1].frameX += frameAdjustment;
+            //Main.tile[i, topY + 2].frameX += frameAdjustment;
+            Wiring.SkipWire(i, topY);
+            Wiring.SkipWire(i, topY + 1);
+            //Wiring.SkipWire(i, topY + 2);
+            NetMessage.SendTileSquare(-1, i, topY + 1, 2, TileChangeType.None);
         }
     }
     
